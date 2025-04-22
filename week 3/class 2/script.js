d8ff0336bc11cd2e3f426ae8f43f5a4018a19c69
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-22 22:27:21 +0500

    full stack web development Week 3 Day 2

---
+++ week 3/class 2/script.js
@@ -0,0 +1,55 @@
+
+// this is the code for printing a
+let a  = 4;
+let b = 5;
+let c =60;
+// a +=5;   // a = a+5
+// a -= 5; // a = a-5
+// a *= 5;
+// a /=5;
+// a %= 3; //a = a%3  => a= 10%3 
+
+
+// power operator
+
+// console.log(a**= b) //  3*3*3*3
+
+
+
+// a--;
+//  console.log(a);
+
+
+ // Conditional Statements
+// if(a!=b){
+//     console.log( "a is not equal to b")
+// }
+
+// if else 
+
+// if(a==b){
+//     console.log("a is equal to b")
+// }else {
+//     console.log("a is not equal to b")
+// }
+
+// if else if
+
+
+// if(c==30){
+//     console.log("c is equal to 30")
+// } else if(c==35){
+//     console.log("c is equal to 35")
+// }else if(c==40){
+//     console.log("c is equal to 40")
+// } else {
+//     console.log("all above conditions are false")
+// }
+
+let number = 30;
+
+if(number%5 == 0){
+    console.log("number is divisible by 5")
+}else{
+    console.log("number is not divisible by 5")
+}
