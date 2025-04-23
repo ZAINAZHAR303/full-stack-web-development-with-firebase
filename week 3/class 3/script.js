42f334b7bdea76bd4c1b8dd1381edb0e5b25c9a8
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-24 00:00:41 +0500

    hrelo

---
+++ week 3/class 3/script.js
@@ -0,0 +1,72 @@
+// let age = 18;
+
+// console.log(age>=18 ? "adult" : "minor")
+// let marks = 24;
+
+// if(marks >= 80  && marks<100){
+//     console.log("A")
+// }else if(marks >= 60 && marks <80){
+//     console.log("B")
+// }else if(marks >= 40 && marks <60){
+//     console.log("C")
+// }else if(marks >=30 && marks <40){
+//     console.log("D")
+// }else{
+//     console.log("F")
+// }
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+
+
+// for(let i=3; i2; i++){
+//     console.log("full stack")
+// }
+
+// let i = 0
+// while(i<=3){
+//     console.log("full stack")
+//     i++
+// }
+
+// let i =0;
+// do {
+//     console.log("full stack")
+//     i++;
+
+// }while(i<0)  // condition check
+
+
+// const fruits = "hello"
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+// for(let i= 0; i<fruits.length; i++){
+//     console.log(fruits[i]) 
+// }
+
+
+// const object = ["fruits", "vegetables", "grains"];
+
+// for (let key in object){
+//     console.log(key, object[key])
+// }
+
+// print all even numbers from 0 to 100
+
+for(let i=0; i<=100; i++){  
+    if(i%2 == 0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+    
+
+
