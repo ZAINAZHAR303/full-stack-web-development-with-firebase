09c0f716a05021c17c5e318b8435b07890dc9448
muhammad zubair <itxzubair45@gmail.com>
2025-04-24 09:56:04 -0700

    class 4 week 3

---
+++ week 3/class 4/script.js
@@ -0,0 +1,32 @@
+let str = "zubair shehzad "
+
+// print 
+
+console.log(str.length)
+console.log(str[12]);
+let exp = "expression insert in a expression "
+let exp2 = `the uper expression inserted ${exp} in the expression `
+// console.log(exp2);
+// console.log(exp)
+
+let upperCaseLetter = "java script "
+let lowerCaseLetter = "JAVa SCRIPT"
+console.log(upperCaseLetter.toUpperCase());
+console.log(lowerCaseLetter.toLowerCase());
+console.log("Trim method ",upperCaseLetter.trim())
+
+let sliceVAriable = "this is slice method"
+// console.log(sliceVAriable.slice(5));
+// console.log(sliceVAriable.slice(0, 7));
+
+
+let string1 = "This is string1"
+let stringReplace = string1.replace("string1", "replaceWith string 1")
+// console.log("Replace the string ",stringReplace);
+
+let string2 = "this is string 2"
+// console.log(string1.concat(string2));
+console.log(string1.charAt(7))
+
+
+
