1177f5b82206b126942f2110bc1a189f3b3b76c6
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-02 23:16:34 +0500

    javascript file of third class

---
+++ week 4/class3/script.js
@@ -0,0 +1,15 @@
+// console.log("hello world")
+
+// alert("hello world")
+
+
+// let paragraph = document.getElementsByClassName("para")
+// let paratag = document.getElementsByTagName("p")
+
+let para = document.querySelector("p")
+
+
+console.log(para.tagName
+// para.style.color = "red"
+// para.forEach(p => p.style.color= "red")
+// console.log(paratag)
