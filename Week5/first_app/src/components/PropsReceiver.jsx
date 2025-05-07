d01a68c052d0937493a0e25d0ea0236fcab7f014
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-07 23:41:52 +0500

    props & props drilling concept in react

---
+++ Week5/first_app/src/components/PropsReceiver.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PropsReceiver = ({value}) => {
+  return (
+    <div>
+        <p>PropsReceiver</p>
+
+        <h1> Name : {value}</h1>
+
+    </div>
+  )
+}
+
+export default PropsReceiver
