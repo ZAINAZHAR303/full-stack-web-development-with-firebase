7336d176e81710cc235cdbbfa34ba66a04bb571e
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-07 23:41:07 +0500

    show response after Form submission

---
+++ Week5/first_app/src/components/AfterSubmission.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AfterSubmission = ({first,second}) => {
+  return (
+    <div>
+        <div>hello world</div>
+        <h1>welcome, your name is {first}</h1>
+        <h1>your email is: {second}</h1>
+    </div>
+  )
+}
+
+export default AfterSubmission
