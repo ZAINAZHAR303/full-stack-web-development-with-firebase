c37bea52359d432d115ba211fca698d7b2962099
muhammad zubair <itxzubair45@gmail.com>
2025-05-14 09:53:51 -0700

    week 6 day 3

---
+++ Week5/nextapp/app/components/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAR33MsMiGtvn9SiPavl2kjVG_BK_FMk8s",
+  authDomain: "fullstackwebcourse-7b1b5.firebaseapp.com",
+  projectId: "fullstackwebcourse-7b1b5",
+  storageBucket: "fullstackwebcourse-7b1b5.firebasestorage.app",
+  messagingSenderId: "1839996522",
+  appId: "1:1839996522:web:040840946818fc6a39b413",
+  measurementId: "G-XLH7NR38TQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
--- Week5/nextapp/app/contact/page.jsx
+++ Week5/nextapp/app/contact/page.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const [postImage , setPostImage]  = useState("")
+  const [description , setDescription]  = useState("")
+  const [title , setTitle]  = useState("")
   return (
-    <div>This is contact page</div>
+    <div>
+      
+      <input type="text"  value={postImage} onChange={(e)=>setPostImage(e.target.value)} placeholder='PostImage'/>
+      <input type="text" value={description}onChange={(e)=>setDescription(e.target.value)} placeholder='Description'/>
+      <input type="text" value={title} placeholder='Title'/>
+      <button>Add Post </button>
+    </div>
   )
 }
 
 export default Contact
+
+
+
+
+
+
+
+
+
+
+
+// import { addDoc, collection } from 'firebase/firestore'
+
+// // ...existing code...
+
+// const createData = async (data) => {
+//   try {
+//     const docRef = await addDoc(collection(db, "your-collection-name"), {
+//       ...data,
+//       createdAt: new Date().toISOString()
+//     });
+//     console.log("Document written with ID: ", docRef.id);
+//     return docRef.id;
+//   } catch (error) {
+//     console.error("Error adding document: ", error);
+//     throw error;
+//   }
+// }
+
+// import { getDoc, getDocs, collection, doc } from 'firebase/firestore'
+
+// // Get all documents from a collection
+// const getAllData = async () => {
+//   try {
+//     const querySnapshot = await getDocs(collection(db, "your-collection-name"));
+//     const documents = [];
+//     querySnapshot.forEach((doc) => {
+//       documents.push({ id: doc.id, ...doc.data() });
+//     });
+//     return documents;
+//   } catch (error) {
+//     console.error("Error getting documents: ", error);
+//     throw error;
+//   }
+// }
+
+// // Get a single document by ID
+// const getDataById = async (documentId) => {
+//   try {
+//     const docRef = doc(db, "your-collection-name", documentId);
+//     const docSnap = await getDoc(docRef);
+    
+//     if (docSnap.exists()) {
+//       return { id: docSnap.id, ...docSnap.data() };
+//     } else {
+//       console.log("No such document!");
+//       return null;
+//     }
+//   } catch (error) {
+//     console.error("Error getting document: ", error);
+//     throw error;
+//   }
+// }
+
+// import { deleteDoc, doc } from 'firebase/firestore'
+
+// // Delete a document by ID
+// const deleteData = async (documentId) => {
+//   try {
+//     await deleteDoc(doc(db, "your-collection-name", documentId));
+//     console.log("Document successfully deleted!");
+//     return true;
+//   } catch (error) {
+//     console.error("Error deleting document: ", error);
+//     throw error;
+//   }
+// }
